f50e9bfe920a7fc76f3a92d08904c10a6768c1da
Woody Lin <wancheng.lin001@umb.edu>
2024-04-04 16:54:00 -0400

    Update index.tsx

--- Random-Generator/app/(tabs)/index.tsx
+++ Random-Generator/app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { Text, View } from '@/components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Example</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
