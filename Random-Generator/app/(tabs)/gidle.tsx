2335353e48d7fdd8f98d5cbc87f9a479b709808f
Woody Lin <wancheng.lin001@umb.edu>
2024-04-04 16:53:57 -0400

    Create gidle.tsx

---
+++ Random-Generator/app/(tabs)/gidle.tsx
@@ -0,0 +1,64 @@
+import { StyleSheet, Button, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+
+// import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+
+export default function TabTwoScreen() {
+
+  // State variable to hold image URL
+  const [imageUrl, setImageUrl] = useState('');
+  const totalImages = 1400;
+
+  useEffect(() => {
+    const randomNumber = Math.floor(Math.random() * totalImages);
+
+    const initialImageUrl = `https://woodylinwc.github.io/Image-Storage/GIDLE/image_${randomNumber}.JPG`;
+
+    setImageUrl(initialImageUrl);
+    // Empty dependency array ensures this effect runs only once, on component mount
+  }, []);
+
+
+  const handleButtonPress = () => {
+    const randomNumber = Math.floor(Math.random() * totalImages);
+
+    const initialImageUrl = `https://woodylinwc.github.io/Image-Storage/GIDLE/image_${randomNumber}.JPG`;
+
+    setImageUrl(initialImageUrl);
+  };
+
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>🌸(G)I-DLE🌟Archive</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      {imageUrl !== '' && <Image source={{ uri: imageUrl}} style={styles.image} />}
+      <Button title="Click Me for More!" onPress={handleButtonPress} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  image: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+    marginBottom: 20,
+  }
+});
