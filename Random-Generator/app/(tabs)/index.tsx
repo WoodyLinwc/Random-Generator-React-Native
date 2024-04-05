7b078193aff682d48c2d43c360af75c2fb65addd
Woody Lin <wancheng.lin001@umb.edu>
2024-04-04 22:33:06 -0400

    Update index.tsx

--- Random-Generator/app/(tabs)/index.tsx
+++ Random-Generator/app/(tabs)/index.tsx
@@ -1,14 +1,23 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Example</Text>
+      <Text style={styles.title}>👾Random Generator</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      
+      <View style={styles.item}>
+        <Text style={styles.text}>1. (G)I-DLE Image</Text>
+      </View>
+
+      <View style={styles.item}>
+        <Text style={styles.text}>2. City Explorer</Text>
+      </View>
+      
+      
+      <Text style={styles.text}>...</Text>
     </View>
   );
 }
@@ -17,7 +26,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 50,
   },
   title: {
     fontSize: 20,
@@ -27,5 +37,15 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  item: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    backgroundColor: '#f0f0f0', 
+    marginBottom: 10, 
   },
 });
